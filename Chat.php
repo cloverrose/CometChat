3fbf4f1e7660aa399c5c6d710c2d23276b2d4d40
cloverrose <clover.plus.rose@gmail.com>
2012-08-09 18:52:07 +0900

    use mysqli

--- Chat.php
+++ Chat.php
@@ -6,58 +6,71 @@ class Chat{
 
     public function __construct(){
         include "db_info.php";
-        $this->link = mysql_connect($host, $username, $password);
-        mysql_select_db($dbname, $this->link);
+        $this->link = new mysqli($host, $username, $password, $dbname);
     }
 
     public function __destruct(){
-        mysql_close($this->link);
+        $this->link->close();
     }
 
     public function count(){
-        $sql = "SELECT COUNT(*) FROM chat;";
-        $result = mysql_query($sql, $this->link);
-        $count = mysql_fetch_row($result);
-        return $count[0];
+        $sql = "SELECT COUNT(*) FROM $this->name;";
+        $result = $this->link->query($sql);
+        $row = $result->fetch_row();
+        $count = $row[0];
+        $result->close();
+        return $count;
     }
 
     public function get_pk($pk){
-        $sql = "SELECT * FROM chat WHERE pk = '$pk';";
-        $result = mysql_query($sql, $this->link);
-        list($pk, $n, $w, $dt) = mysql_fetch_row($result);
-        $speak = array('pk' => $pk, 'nick' => $n, 'words' => $w, 'dt' => $dt);
+        $sql = "SELECT * FROM $this->name WHERE pk = '$pk';";
+        $result = $this->link->query($sql);
+        if($row = $result->fetch_assoc()){
+            $speak = array('pk' => $row['pk'],
+                           'nick' => $row['nick'],
+                           'words' => $row['words'],
+                           'dt' => $row['dt']);
+        }else{
+            $speak = null;
+        }
+        $result->close();
         return $speak;   
     }
 
     public function find_post($dt, $pre_pk){
-        $sql = "SELECT * FROM chat WHERE dt >= '$dt' ORDER BY dt;";
-        $result = mysql_query($sql, $this->link);
-        $rows = mysql_num_rows($result);
-        $post = null;
-        for($i=1;$i<=$rows; $i++) {
-            list($pk, $n, $w, $dt) = mysql_fetch_row($result);
-            if($pk != $pre_pk){
-                return array('pk' => $pk, 'nick' => $n, 'words' => $w, 'dt' => $dt);
+        $sql = "SELECT * FROM $this->name WHERE dt >= '$dt' ORDER BY dt;";
+        $result = $this->link->query($sql);
+        while($row = $result->fetch_assoc()){
+            $speak = array('pk' => $row['pk'],
+                           'nick' => $row['nick'],
+                           'words' => $row['words'],
+                           'dt' => $row['dt']);
+            if($speak['pk'] != $pre_pk){
+                $result->close();
+                return $speak;
             }
         }
+        $result->close();
         return null;
     }
 
     public function select_dict(){
-        $sql = "SELECT * FROM chat ORDER BY dt;";
-        $result = mysql_query($sql, $this->link);
-        $rows = mysql_num_rows($result);
+        $sql = "SELECT * FROM $this->name ORDER BY dt;";
+        $result = $this->link->query($sql);
         $speaks = array();
-        for($i=1;$i<=$rows; $i++) {
-            list($pk, $n, $w, $dt) = mysql_fetch_row($result);
-            $speaks[] = array('pk' => $pk, 'nick' => $n, 'words' => $w, 'dt' => $dt);
+        while($row = $result->fetch_assoc()) {
+            $speaks[] = array('pk' => $row['pk'],
+                              'nick' => $row['nick'],
+                              'words' => $row['words'],
+                              'dt' => $row['dt']);
         }
+        $result->close();
         return $speaks;
-    }
+   }
 
     public function insert($nick, $words, $dt){
-        $sql = "INSERT INTO chat(nick, words, dt) values ('$nick', '$words', '$dt');";
-        mysql_query($sql, $this->link);
+        $sql = "INSERT INTO $this->name (nick, words, dt) values ('$nick', '$words', '$dt');";
+        $this->link->query($sql);
     }
 }
 ?>
