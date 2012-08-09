562704c32a74642e10133dc22551d6b4f6ba32ce
cloverrose <clover.plus.rose@gmail.com>
2012-08-09 18:30:48 +0900

    use mysqli

--- User.php
+++ User.php
@@ -6,38 +6,48 @@ class User{
 
     public function __construct(){
         include "db_info.php";
-        $this->link = mysql_connect($host, $username, $password);
-        mysql_select_db($dbname, $this->link);
+        $this->link = new mysqli($host, $username, $password, $dbname);
     }
 
     public function __destruct(){
-        mysql_close($this->link);
+        $this->link->close();
     }
 
     public function count(){
         $sql = "SELECT COUNT(*) FROM $this->name;";
-        $result = mysql_query($sql, $this->link);
-        $count = mysql_fetch_row($result);
-        return $count[0];
+        $result = $this->link->query($sql);
+        $row = $result->fetch_row();
+        $count = $row[0];
+        $result->close();
+        return $count;
     }
 
     public function get_pk($pk){
         $sql = "SELECT * FROM $this->name WHERE pk = '$pk';";
-        $result = mysql_query($sql, $this->link);
-        list($pk, $r, $n, $dt) = mysql_fetch_row($result);
-        $user = array('pk' => $pk, 'room' => $r, 'nick' => $n, 'dt' => $dt);
+        $result = $this->link->query($sql);
+        if($row = $result->fetch_assoc()){
+            $user = array('pk' => $row['pk'],
+                          'room' => $row['room'],
+                          'nick' => $row['nick'],
+                          'dt' => $row['dt']);
+        }else{
+            $user = null;
+        }
+        $result->close();
         return $user;   
     }
 
     public function get_room($room){
         $sql = "SELECT * FROM $this->name WHERE room = '$room' ORDER BY dt;";
-        $result = mysql_query($sql, $this->link);
-        $rows = mysql_num_rows($result);
+        $result = $this->link->query($sql);
         $users = array();
-        for($i=1;$i<=$rows; $i++) {
-            list($pk, $r, $n, $dt) = mysql_fetch_row($result);
-            $users[] = array('pk' => $pk, 'room' => $r, 'nick' => $n, 'dt' => $dt);
+        while($row = $result->fetch_assoc()){
+            $users[] = array('pk' => $row['pk'],
+                             'room' => $row['room'],
+                             'nick' => $row['nick'],
+                             'dt' => $row['dt']);
         }
+        $result->close();
         return $users;
     }
 
@@ -55,31 +65,34 @@ class User{
 
     public function is_login($room, $nick){
         $sql = "SELECT * FROM $this->name WHERE room = '$room' and nick = '$nick' ORDER BY dt;";
-        $result = mysql_query($sql, $this->link);
-        $rows = mysql_num_rows($result);
-        return $rows != 0;
+        $result = $this->link->query($sql);
+        $row_cnt = $result->num_rows();
+        $result->close();
+        return $row_cnt != 0;
     }
 
     public function select_dict(){
         $sql = "SELECT * FROM $this->name ORDER BY dt;";
-        $result = mysql_query($sql, $this->link);
-        $rows = mysql_num_rows($result);
+        $result = $this->link->query($sql);
         $users = array();
-        for($i=1;$i<=$rows; $i++) {
-            list($pk, $r, $n, $dt) = mysql_fetch_row($result);
-            $users[] = array('pk' => $pk, 'room' => $r, 'nick' => $n, 'dt' => $dt);
+        while($row = $result->fetch_assoc()) {
+            $users[] = array('pk' => $row['pk'],
+                             'room' => $row['room'],
+                             'nick' => $row['nick'],
+                             'dt' => $row['dt']);
         }
+        $result->close();
         return $users;
     }
   
     public function insert($room, $nick, $dt){
         $sql = "INSERT INTO $this->name (room, nick, dt) values ('$room', '$nick', '$dt');";
-        mysql_query($sql, $this->link);
+        $this->link->query($sql);
     }
 
     public function delete($room, $nick){
         $sql = "DELETE FROM $this->name WHERE room = '$room' and nick = '$nick';";
-        mysql_query($sql, $this->link);
+        $this->link->query($sql);
     }
 }
 ?>
