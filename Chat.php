a98f6eb16ff8d25b5c97caeead30ef00d585ccc2
cloverrose <clover.plus.rose@gmail.com>
2012-08-11 10:02:32 +0900

    add colomn room in chat

--- Chat.php
+++ Chat.php
@@ -10,7 +10,8 @@ class Chat extends Model{
         return array('pk' => $row['pk'],
                      'nick' => htmlspecialchars($row['nick'], ENT_QUOTES, 'UTF-8'),
                      'words' => htmlspecialchars($row['words'], ENT_QUOTES, 'UTF-8'),
-                     'dt' => $row['dt']);
+                     'dt' => $row['dt'],
+                     'room' => htmlspecialchars($row['room'], ENT_QUOTES, 'UTF-8'));
     }
 
     public function find_post($dt, $pre_pk){
@@ -20,6 +21,13 @@ class Chat extends Model{
         return $ret;
     }
 
+    public function filter_room($room){
+        $stmt = $this->link->prepare("SELECT * FROM $this->name WHERE room = ? ORDER BY dt, pk");
+        $stmt->bind_param("s", $room);
+        $rets = $this->safefilter($stmt);
+        return $rets;
+    }
+
     public function get_max_pk(){
         $sql = "SELECT * FROM $this->name ORDER BY pk DESC LIMIT 1;";
         $ret = $this->get($sql);
@@ -27,9 +35,9 @@ class Chat extends Model{
         return $pk;
     }
 
-    public function insert($nick, $words, $dt){
-        $stmt = $this->link->prepare("INSERT INTO $this->name (nick, words, dt) values (?, ?, ?)");
-        $stmt->bind_param("sss", $nick, $words, $dt);
+    public function insert($nick, $words, $dt, $room){
+        $stmt = $this->link->prepare("INSERT INTO $this->name (nick, words, dt, room) values (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $nick, $words, $dt, $room);
         $stmt->execute();
         $stmt->close();
     }
